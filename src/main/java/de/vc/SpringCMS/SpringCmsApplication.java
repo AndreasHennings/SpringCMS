80403e748c81941d1ed8f34f8f1c8de0b223246e
AndreasHennings <a3nd@gmx.de>
2022-04-24 11:01:51 +0200

    Initial commit

---
+++ src/main/java/de/vc/SpringCMS/SpringCmsApplication.java
@@ -0,0 +1,21 @@
+package de.vc.SpringCMS;
+
+import de.vc.SpringCMS.student.Student;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@SpringBootApplication
+public class SpringCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCmsApplication.class, args);
+	}
+
+
+}
